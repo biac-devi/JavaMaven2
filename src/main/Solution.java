0796d628e41bac9833f00cd92d6c6c4d85c4e9e1
teargirl <talentdby@gmail.com>
2021-09-05 01:01:37 -0700

    commit 1

---
+++ src/main/Solution.java
@@ -0,0 +1,195 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        char[][] grid1 =
+                {
+                        {'.', '#'},
+                        {'.', '.'},
+                        {'#', '.'}
+                };
+        System.out.println(isWithinKSec(grid1, 4));
+
+        char[][] grid2 =
+                {
+                        {'.', '#'},
+                        {'.', '#'},
+                        {'#', '.'}
+                };
+        System.out.println(isWithinKSec(grid2, 10));
+
+        char[][] grid3 =
+                {
+                        {'.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.'}
+                };
+        System.out.println(isWithinKSec(grid3, 1000));
+
+        char[][] grid4 =
+                {
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}
+                };
+        System.out.println("Test case #4:");
+        System.out.println(isWithinKSec(grid4, 26));
+        System.out.println(isWithinKSec(grid4, 27));
+
+        char[][] grid5 =
+                {
+                        {'.', '#', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#'},
+                        {'.', '#', '#', '.', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'.', '#', '#', '.', '.', '.', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'.', '#', '#', '#', '#', '.', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#'},
+                        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.'}
+                };
+        System.out.println("Test case #5:");
+        //In this case, length of the path is 39.
+        System.out.println(isWithinKSec2(grid5, 38));
+        System.out.println(isWithinKSec2(grid5, 39));
+    }
+
+    public static boolean isWithinKSec2(char[][] grid, int k){
+        int[][] result = new int[grid.length][grid[0].length];
+        for (int i = 0; i < result.length; i++) {
+            Arrays.fill(result[i], 9999999);
+        }
+
+        result[0][0] = 1;
+        boolean changed = true;
+        int iteration = 0;
+
+        while(changed){
+            changed = false;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    // check the up cell
+
+                    if (i > 0 && grid[i - 1][j] == '.') {
+                        if (result[i - 1][j] > result[i][j] + 1) {
+                            result[i - 1][j] = result[i][j] + 1;
+                            //very important! If there is an around cell updated, then we need to check that updated cell recursively! Please refer my test case #5.
+                            changed = true;
+                        }
+                    }
+                    // check the down cell
+                    if (i + 1 < grid.length && grid[i + 1][j] == '.') {
+                        if (result[i + 1][j] > result[i][j] + 1) {
+                            result[i + 1][j] = result[i][j] + 1;
+                            //If there is an around cell updated, then we need to check that updated cell recursively!
+                            changed = true;
+                        }
+                    }
+                    // check the left cell
+                    if (j > 0 && grid[i][j - 1] == '.') {
+                        if (result[i][j - 1] > result[i][j] + 1) {
+                            result[i][j - 1] = result[i][j] + 1;
+                            //If there is an around cell updated, then we need to check that updated cell recursively!
+                            changed = true;
+                        }
+                    }
+                    // check the right cell
+                    if (j + 1 < grid[0].length && grid[i][j + 1] == '.') {
+                        if (result[i][j + 1] > result[i][j] + 1) {
+                            result[i][j + 1] = result[i][j] + 1;
+                            //If there is an around cell updated, then we need to check that updated cell recursively!
+                            changed = true;
+                        }
+                    }
+                }
+            }
+            iteration++;
+        }
+
+        System.out.println("Iteration: " + iteration);
+        return result[grid.length - 1][grid[0].length - 1] <= k ? true : false;
+    }
+
+
+    public static boolean isWithinKSec(char[][] grid, int k) {
+        //Dynamic Programming. result[][] to store the min steps (in seconds) from left-top to the corresponding element in each iteration.
+        int[][] result = new int[grid.length][grid[0].length];
+        //Initial the DP matrix.
+        for (int i = 0; i < result.length; i++) {
+            Arrays.fill(result[i], 9999999);
+        }
+        //Initial Top-left cell.
+        result[0][0] = 1;
+        //Fill the value to build the DP matrix
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                dpHelper(i, j, grid, result);
+            }
+        }
+        // return true if within k seconds. (include k)
+        return result[grid.length - 1][grid[0].length - 1] <= k ? true : false;
+
+    }
+
+    public static void dpHelper(int i, int j, char[][] grid, int[][] result) {
+        int tmp = 0;
+        // check the up cell
+        if (i > 0 && grid[i - 1][j] == '.') {
+            if (result[i - 1][j] > result[i][j] + 1) {
+                result[i - 1][j] = result[i][j] + 1;
+                //very important! If there is an around cell updated, then we need to check that updated cell recursively! Please refer my test case #5.
+                dpHelper(i - 1, j, grid, result);
+            }
+        }
+        // check the down cell
+        if (i + 1 < grid.length && grid[i + 1][j] == '.') {
+            if (result[i + 1][j] > result[i][j] + 1) {
+                result[i + 1][j] = result[i][j] + 1;
+                //If there is an around cell updated, then we need to check that updated cell recursively!
+                dpHelper(i + 1, j, grid, result);
+            }
+        }
+        // check the left cell
+        if (j > 0 && grid[i][j - 1] == '.') {
+            if (result[i][j - 1] > result[i][j] + 1) {
+                result[i][j - 1] = result[i][j] + 1;
+                //If there is an around cell updated, then we need to check that updated cell recursively!
+                dpHelper(i, j - 1, grid, result);
+            }
+        }
+        // check the right cell
+        if (j + 1 < grid[0].length && grid[i][j + 1] == '.') {
+            if (result[i][j + 1] > result[i][j] + 1) {
+                result[i][j + 1] = result[i][j] + 1;
+                //If there is an around cell updated, then we need to check that updated cell recursively!
+                dpHelper(i, j + 1, grid, result);
+            }
+        }
+
+        return;
+    }
+}
---
+++ src/main/Syntax.java
@@ -0,0 +1,70 @@
+
+
+public class Syntax {
+    public static void main(String[] args) {
+        int[] input1 = {1,3,3,7,7};
+        int[] input2 = {1,3,3,7,7,8,8};
+        int[] input3 = {1,1,3,7,7,9,9};
+        int[] input4 = {1,1,7,7,9,9,11,22,22};
+        int[] input5 = {1,1,7,7,9,9,22,22,33};
+        int[] input6 = {1,1,7,7,8,9,9,22,22};
+        int[] input7 = {5};
+        int[] input8 = {};
+        int[] input9 = null;
+        System.out.println(findunique(input1));
+        System.out.println(findunique(input2));
+        System.out.println(findunique(input3));
+        System.out.println(findunique(input4));
+        System.out.println(findunique(input5));
+        System.out.println(findunique(input6));
+        System.out.println(findunique(input7));
+        System.out.println(findunique(input8));
+        System.out.println(findunique(input9));
+    }
+
+    static int findunique(int[] input)
+    {
+        if(input == null) return -1;
+        if(input.length == 0) return -1;
+        if(input.length == 1) return input[0];
+
+        int begin = 0;
+        int end = input.length - 1;
+        int length = end + 1;
+        int mid = end/2;
+        while(begin < end)
+        {
+            if(((length-1)/2) % 2 == 0)
+            {
+                if(input[mid] != input[mid + 1] && input[mid] != input[mid - 1])
+                    return input[mid];
+                if(input[mid] == input[mid + 1])
+                {
+                    begin = mid;
+                }
+                else if (input[mid] == input[mid - 1])
+                {
+                    end = mid;
+                }
+            }
+            else
+            {
+                if(input[mid] != input[mid + 1] && input[mid] != input[mid - 1])
+                    return input[mid];
+                if(input[mid] == input[mid + 1])
+                {
+                    end = mid -1 ;
+
+                }
+                else if (input[mid] == input[mid - 1])
+                {
+                    begin = mid + 1;
+                }
+            }
+            mid = (begin + end ) /2;
+            length = end - begin + 1;
+        }
+
+        return input[mid];
+    }
+}

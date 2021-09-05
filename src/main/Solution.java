01c63bca4b7e2d86a9a885731bb0f58c876d2c39
teargirl <talentdby@gmail.com>
2021-09-05 01:56:44 -0700

    com4

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -55,7 +55,6 @@ public class Solution {
         System.out.println("Test case #4:");
 
         System.out.println(isWithinKSec(grid4, 26));
-
         System.out.println(isWithinKSec(grid4, 27));
 
         char[][] grid5 =
